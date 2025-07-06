3cef6a0b81e8b3df78579d3fc5bcc3215fe3a44a
Penguin-SAMA <496107146@qq.com>
2025-07-06 19:35:04 +0800

    update

---
+++ lab/lab2/kernel/bio.c
@@ -0,0 +1,135 @@
+// 文件系统的块缓存管理
+
+// 接口：
+// * 要获取特定磁盘块的缓冲区，调用 bread。
+// * 修改缓冲区数据后，调用 bwrite 将其写入磁盘。
+// * 使用完缓冲区后，调用 brelse。
+// * 调用 brelse 后不要再使用该缓冲区。
+// * 同一时间只能有一个进程使用缓冲区，
+//     所以不要比必要时间更长地持有它们。
+
+#include "types.h"
+#include "param.h"
+#include "spinlock.h"
+#include "sleeplock.h"
+#include "riscv.h"
+#include "defs.h"
+#include "fs.h"
+#include "buf.h"
+
+struct {
+    struct spinlock lock;
+    struct buf buf[NBUF];
+
+    // Linked list of all buffers, through prev/next.
+    // Sorted by how recently the buffer was used.
+    // head.next is most recent, head.prev is least.
+    struct buf head;
+} bcache;
+
+void binit(void) {
+    struct buf* b;
+
+    initlock(&bcache.lock, "bcache");
+
+    // Create linked list of buffers
+    bcache.head.prev = &bcache.head;
+    bcache.head.next = &bcache.head;
+    for (b = bcache.buf; b < bcache.buf + NBUF; b++) {
+        b->next = bcache.head.next;
+        b->prev = &bcache.head;
+        initsleeplock(&b->lock, "buffer");
+        bcache.head.next->prev = b;
+        bcache.head.next = b;
+    }
+}
+
+// Look through buffer cache for block on device dev.
+// If not found, allocate a buffer.
+// In either case, return locked buffer.
+static struct buf*
+bget(uint dev, uint blockno) {
+    struct buf* b;
+
+    acquire(&bcache.lock);
+
+    // Is the block already cached?
+    for (b = bcache.head.next; b != &bcache.head; b = b->next) {
+        if (b->dev == dev && b->blockno == blockno) {
+            b->refcnt++;
+            release(&bcache.lock);
+            acquiresleep(&b->lock);
+            return b;
+        }
+    }
+
+    // Not cached.
+    // Recycle the least recently used (LRU) unused buffer.
+    for (b = bcache.head.prev; b != &bcache.head; b = b->prev) {
+        if (b->refcnt == 0) {
+            b->dev = dev;
+            b->blockno = blockno;
+            b->valid = 0;
+            b->refcnt = 1;
+            release(&bcache.lock);
+            acquiresleep(&b->lock);
+            return b;
+        }
+    }
+    panic("bget: no buffers");
+}
+
+// Return a locked buf with the contents of the indicated block.
+struct buf*
+bread(uint dev, uint blockno) {
+    struct buf* b;
+
+    b = bget(dev, blockno);
+    if (!b->valid) {
+        virtio_disk_rw(b, 0);
+        b->valid = 1;
+    }
+    return b;
+}
+
+// Write b's contents to disk.  Must be locked.
+void bwrite(struct buf* b) {
+    if (!holdingsleep(&b->lock))
+        panic("bwrite");
+    virtio_disk_rw(b, 1);
+}
+
+// Release a locked buffer.
+// Move to the head of the most-recently-used list.
+void brelse(struct buf* b) {
+    if (!holdingsleep(&b->lock))
+        panic("brelse");
+
+    releasesleep(&b->lock);
+
+    acquire(&bcache.lock);
+    b->refcnt--;
+    if (b->refcnt == 0) {
+        // no one is waiting for it.
+        b->next->prev = b->prev;
+        b->prev->next = b->next;
+        b->next = bcache.head.next;
+        b->prev = &bcache.head;
+        bcache.head.next->prev = b;
+        bcache.head.next = b;
+    }
+
+    release(&bcache.lock);
+}
+
+void bpin(struct buf* b) {
+    acquire(&bcache.lock);
+    b->refcnt++;
+    release(&bcache.lock);
+}
+
+void bunpin(struct buf* b) {
+    acquire(&bcache.lock);
+    b->refcnt--;
+    release(&bcache.lock);
+}
